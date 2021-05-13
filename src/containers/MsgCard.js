f9ab29f14bf3beb9d08ea199723cf70271e4b7a8
leosole <8992517+leosole@users.noreply.github.com>
2021-05-12 22:55:17 -0300

    time correction

--- src/containers/MsgCard.js
+++ src/containers/MsgCard.js
@@ -40,11 +40,19 @@ export default class MsgCard extends React.Component {
     const present = new Date();
     const date = new Date(msg.createdAt);
     var passed = (present.getTime() - date.getTime())/(1000*60*60*24);
-    if(passed>1){
-      return 'Para: '+msg.name+', a '+Math.ceil(passed)+' dias'
+    if(passed<1){
+      const minutes = Math.ceil(passed*60);
+      const plural = minutes === 1? ' minuto':' minutos';
+      return 'Para: '+msg.name+', a '+minutes+plural
+    }
+    if(passed>=1){
+      const days = Math.ceil(passed);
+      const plural = days === 1? ' dia':' dias';
+      return 'Para: '+msg.name+', a '+days+plural
     } else{
-      passed *= 24;
-      return 'Para: '+msg.name+', a '+Math.ceil(passed)+' horas'
+      const hours = Math.ceil(passed*24);
+      const plural = hours === 1? ' hora':' horas';
+      return 'Para: '+msg.name+', a '+hours+plural
     }
   }
 
